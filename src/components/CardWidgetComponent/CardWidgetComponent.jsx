363ff34487851c8980da8d5d36b468dd4bf404d8
Lolitoox <guidobasano@hotmail.com>
2024-02-19 20:57:05 -0300

    Correcciones generales

--- src/components/CardWidgetComponent/CardWidgetComponent.jsx
+++ src/components/CardWidgetComponent/CardWidgetComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useCart } from '../context/cartContext';
 import { Link } from 'react-router-dom';
 import Badge from 'react-bootstrap/Badge';
-import { IoCartOutline } from "react-icons/io5";
+import { BsCart } from "react-icons/bs";
 
 const CartWidgetComponent = () => {
     const { totalQuantity } = useCart();
@@ -10,7 +10,7 @@ const CartWidgetComponent = () => {
     return (
         <Link to="/cart" className="cart-widgets">
             <div className="cart-widgets__shopping-bag">
-                <IoCartOutline size={30} />
+                <BsCart size={30} />
                 {totalQuantity > 0 && (
                     <Badge bg="danger" className="cart-widgets__items-counter">
                         {totalQuantity}
